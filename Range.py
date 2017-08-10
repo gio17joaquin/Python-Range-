5fc8b381e61a03133efcddffe98f0fc207bc36da
gio17joaquin <gio17joaquin@gmail.com>
2017-08-09 23:59:18 -0700

    first commit

---
+++ Range.py
@@ -0,0 +1,14 @@
+#Drill 1
+for i in range(4):
+    print(i)
+print ('done')
+
+#Drill 2
+for i in range(3, -1, -1):
+    print(i)
+print ('done')
+
+#Drill 3
+for i in range(8, 0, -2):
+    print(i)
+print ('done')
